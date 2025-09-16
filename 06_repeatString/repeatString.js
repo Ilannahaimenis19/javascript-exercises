1ad23798a81919e8d6302bfc4f5ffebe74846368
ilannahaimenis19 <188930393+Ilannahaimenis19@users.noreply.github.com>
2025-09-16 19:11:17 -0300

    Complete exercises from 01 to 06

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) { 
+    let string = '';
+    for (let i; i < num; i++) {
+       string += word;
+    } 
+    return string;
 };
 
 // Do not edit below this line

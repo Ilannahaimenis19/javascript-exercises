4f340110db2af2a78c9d606b80311aecb4846258
ilannahaimenis19 <188930393+Ilannahaimenis19@users.noreply.github.com>
2025-09-18 16:27:06 -0300

    Complete exercise 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    const smallest = Math.min(num1, num2);
+    const biggest = Math.max(num1, num2);
+    let numbers = [];
+        for (let num = smallest; num <= biggest; num++) {
+            numbers.push(num)
+        } 
+        return numbers.reduce((acc, num) => acc + num, 0);
 };
-
 // Do not edit below this line
 module.exports = sumAll;
